087fb77ad74e454f4efb792c6e53e39d1520c96e
ysmnadm <yasmine.adame@gmail.com>
2015-09-17 19:00:29 -0700

    this is my second commit

---
+++ script.js
@@ -0,0 +1,258 @@
+
+
+/*
+    Assignment1
+    COMM644
+    Yasmine Adame
+ */
+
+//  Q1
+
+var someMonth;
+function theMonth(){}		                                     // function to return current month
+CURRENT_MONTH : September; 		                         // a constant
+var summerMonth = [ "June", "August"]; 		         // an array of summer months
+function myLibraryaFunction(){}	                                 // a function 
+
+//  Q2 
+
+// numeric literal
+
+// 787
+
+//  string literal
+
+/* 
+"United State";
+console.log("united State" + " is a 2nd biggest state in US in an area wise.");
+*/
+
+//  boolean literal
+/*
+false;
+console.log(false + " is boolean Literal in JavaScript");
+*/
+//  string literal
+/*
+null;    // represent an empty value
+*/
+    
+
+// Q3   
+
+
+var complexExpression1 = ((10 + 2)*3.5)/(2+2);
+
+var complexExpression2 = (Area + 2/Area) + 5*2;
+
+
+
+
+// Q4 
+
+/* ===========================================
+var s_FirstName;
+var s_LastName;
+var n_Address;
+var s_City;
+var c_State;
+var n_ZipCode
+var n_YourAge;
+var s_ReferralResource;
+var s_MayWeContactYou;
+
+*/===========================================
+
+// Q5 
+
+/*
+var sFirstName = "Yasmine";
+var sLastName = "Adame";
+var sCity = "San Diego";  
+
+*/
+
+// Q6
+
+
+/*
+var yearsOfExperience = 15;
+console.log("I have " + yearsOfExperience + " in Marketing.");
+*/
+
+/*
+var secondVariable = true;
+console.log("I did have pass the exam in first attempt is " + secondVariable +".");
+*/
+
+
+var thirdVaraible = 500;
+console.log("I worked with fortune " + thirdVariable + " company is" true);
+
+
+// Q7 
+
+/*
+car = "Ford";
+var make;
+console.log(car);
+*/
+
+
+//  Q8 
+
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'\m not sure about the former."';
+console.log(someString);
+*/
+
+// Q9
+
+/*=================================================
+// Null Variable
+var a = 1;
+var b = null;
+var a = b;
+console.log(a);
+*/
+    
+
+// Undefined Variable
+/*
+var a;
+console.log(a);
+*/
+    
+    
+// Q10
+
+/* ==============================================
+typeof x = "Yasmine";
+console.log(x);
+
+typeof y = 3.14;
+console.log(y);
+
+typeof z = false;
+console.log(z);
+
+
+typeof new Date();
+console.log(Date());
+
+typeof myPet;
+console.log(myPet)
+*/
+
+// Q11
+
+/* 
+
+alert("Hello" + "Zak Ruvaicaba" + ",  welcome to the JavaScript class!");
+*/ 
+
+// Q12
+/*
+var name = "Yasmine Adame";
+alert("Hello" + name + ",  welcome to the JavaScript class!");
+*/ 
+
+// Q13
+
+/*
+var name = "Yasmine Adame";
+var course = "JavaScript";
+
+alert("Hello" + name + ",  welcome to the " + course + " class!");
+*/
+    
+// Q14
+alert("Hello Yasmine.\n Welcome to the JavaScript class!");
+
+
+// Q15 
+var name;
+prompt("Please enter your name!");
+
+//  Q16
+var course;
+prompt("What class are you taking in this semester?")
+
+// Q17
+
+var x = 10;
+var y = 20;
+var sum = x + y;
+console.log(sum);
+
+
+// Q18
+
+var x = 20;
+x += 20;
+console.log(x);
+
+// 19
+
+var x = 20;
+x *= 5;
+console.log(x);
+
+// 20
+var x = 20 % 3;
+x = x/1;
+console.log(x);
+
+
+// Q20
+
+/*
+// Comparsion Operators
+
+var x = 2;
+var y = 5;
+console.log(x < y);
+
+var x = 2;
+var y = 5;
+console.log(x <= y);
+
+var x = 5;
+var y = 5;
+console.log(x == y);
+*/
+
+// Logical Operators
+
+/*
+var x = 15;
+var y = 15;
+console.log(x == 5 && y == 5);
+
+var x = 2;
+var y = 5;
+console.log(x == 2 || y == 5);
+
+var x = 2;
+var y = 15;
+console.log(x <= y != true);
+*/
+
+
+// Q21 
+
+var x = 2;
+var y = 5;
+console.log(x > y);
+
+var x = 2;
+var y = 5;
+console.log(x >= y);
+
+var x = 5;
+var y = 15;
+console.log(x == y);
+*/
+
